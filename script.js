b5f9c1dbed264006f571d2b530db1c39140e6a07
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-10-15 15:52:54 -0400

    Initialize webexCCWidget on window load

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  window.webexCCWidget.init({
+    orgId: "YOUR_ORG_ID",
+    tenantId: "YOUR_TENANT_ID",
+    flowId: "YOUR_FLOW_ID",
+    channelId: "YOUR_CHANNEL_ID",
+  });
+};
